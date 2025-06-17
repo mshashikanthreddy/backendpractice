edfecdaa4dcaf5e94a176c1227f67cdf5777639d
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2025-06-17 10:44:23 +0530

    body parsing form data

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express"); 
 const app = express();
+const bodyParser = requiure('body-parser');
 
+app.use(bodyParser.urlencoded({extended: false})); // used for parsing form data not for files,imagees
+//Use extended: false if you only expect simple key-value pairs.
+//Use extended: true if you expect nested objects or arrays.
 app.use('/add-product',(req,res,next)  => {
 
     console.log('In the middleware');
-    res.send('<h1>The "add product" page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button><form>');
+});
+
+app.use("/product",(req,res,next) => {
+
+    console.log(req.body);
+    res.redirect('/');
+})
     
-}); // we have to add paths on priority basis as requests goes from top to bottom
+ // we have to add paths on priority basis as requests goes from top to bottom
 // use "next" only if you want to go to next requests.
 
 app.use('/',(req,res,next) => {
