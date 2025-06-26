acc96f4c2484427bcd475ceafa90d522c26c5a74
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2025-06-26 12:40:40 +0530

    using req,res

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const http = require('http'); // by using "require" we can import our own files,
 // http.createServer(rqListener);
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+
+    console.log(req.url, req.method ,req.headers);
+    //process.exit(); //hard exit and close server
+    res.setHeader('Content-Type','text/html'); // default header will be a text/html
+    res.write('<html>');
+    res.write('<head><title> my first page</title></head>');
+    res.write('<body><h1>hello from my Node.js server</h1></body>');
+    res.write('</html>');
+    res.end();
+
 })
 //createServer method actually returns a Server.
 
